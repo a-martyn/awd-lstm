f31a9d8a3da5b54c3ad70ff6bf9cc3ded9cb54ba
a-martyn <a-martyn@users.noreply.github.com>
2019-01-23 16:26:48 +0000

    occams razor

--- model/net.py
+++ model/net.py
@@ -60,20 +60,20 @@ class LSTMCell(nn.Module):
     and activations between cells.
     """
 
-    def __init__(self, input_size, hidden_size, bias=True):
+    def __init__(self, input_size, output_size, bias=True):
         super(LSTMCell, self).__init__()
         
         # Contains all weights for the 4 linear mappings of the input x
         # e.g. Wi, Wf, Wo, Wc
-        self.i2h = nn.Linear(input_size, 4*hidden_size, bias=bias)
+        self.i2h = nn.Linear(input_size, 4*output_size, bias=bias)
         # Contains all weights for the 4 linear mappings of the hidden state h
         # e.g. Ui, Uf, Uo, Uc
-        self.h2h = nn.Linear(hidden_size, 4*hidden_size, bias=bias)
-        self.hidden_size = hidden_size
+        self.h2h = nn.Linear(output_size, 4*output_size, bias=bias)
+        self.output_size = output_size
         self.reset_parameters()
 
     def reset_parameters(self):
-        std = 1.0 / math.sqrt(self.hidden_size)
+        std = 1.0 / math.sqrt(self.output_size)
         for w in self.parameters():
             w.data.uniform_(-std, std)
 
@@ -81,14 +81,14 @@ class LSTMCell(nn.Module):
         # unpack tuple (recurrent activations, recurrent cell state)
         h, c = hidden
 
-        # Linear mappings : all four in one hit
+        # Linear mappings : all four in one vectorised computation
         preact = self.i2h(x) + self.h2h(h)
 
         # Activations
-        i = th.sigmoid(preact[:, :self.hidden_size])                      # input gate
-        f = th.sigmoid(preact[:, self.hidden_size:2*self.hidden_size])    # forget gate
-        g = th.tanh(preact[:, 3*self.hidden_size:])                       # cell gate
-        o = th.sigmoid(preact[:, 2*self.hidden_size:3*self.hidden_size])  # ouput gate
+        i = th.sigmoid(preact[:, :self.output_size])                      # input gate
+        f = th.sigmoid(preact[:, self.output_size:2*self.output_size])    # forget gate
+        g = th.tanh(preact[:, 3*self.output_size:])                       # cell gate
+        o = th.sigmoid(preact[:, 2*self.output_size:3*self.output_size])  # ouput gate
 
 
         # Cell state computations: 
@@ -117,23 +117,42 @@ class AWD_LSTM(nn.Module):
         self.embedding = nn.Embedding(ntokens, embedding_size)
         self.layer0 = LSTMCell(embedding_size, hidden_size, bias=bias)
         self.layer1 = LSTMCell(hidden_size, hidden_size, bias=bias)
-        self.layer2 = LSTMCell(hidden_size, hidden_size, bias=bias)
-        self.decoder = nn.Linear(hidden_size, ntokens)
-
-        self.varidrop_h = VariationalDropout(p=dropout)
+        self.layer2 = LSTMCell(hidden_size, embedding_size, bias=bias)
+        self.decoder = nn.Linear(embedding_size, ntokens)
 
         self.nlayers = 3
         self.hidden_size = hidden_size
-        self.dropout = dropout
-        self.dropoute = 0.1
+        self.embedding_size = embedding_size
         self.device = device
+
+        # Store activations for AR and TAR regularisation
+        #TAR
+#         self.output = None
+#         self.output_nodrop = None
+
+        # Dropout
+        # TODO: expose dropout settings to api
+        # QUESTION: How did authors arrive at these dropout parameters?
+        self.dropout_wts = 0.5
+        self.dropout_emb = 0.1
+        self.dropout_inp = 0.4
+        self.dropout_hid = 0.25
+        self.varidrop_inp = VariationalDropout(p=self.dropout_inp)
+        self.varidrop_hid = VariationalDropout(p=self.dropout_hid)
+        self.varidrop_out = VariationalDropout(p=self.dropout_hid)
+
+        # Weight tying
+        # https://arxiv.org/abs/1608.05859
+        # https://arxiv.org/abs/1611.01462
+        #self.decoder.weight = self.embedding.weight
+
 
     def init_hiddens(self, batch_size):
         weight = next(self.parameters())
         return (weight.new_zeros(self.nlayers, batch_size, self.hidden_size),
                 weight.new_zeros(self.nlayers, batch_size, self.hidden_size))
 
-    def drop_connect(self, p=0.5):
+    def weight_dropout(self, p=0.5):
         """
         Applies recurrent regularization through a DropConnect mask on the
         hidden-to-hidden recurrent weights as described here:
@@ -146,7 +165,7 @@ class AWD_LSTM(nn.Module):
         self.load_state_dict(sd)
         return
 
-    def embedded_dropout(self, embed, words, p=0.1):
+    def embedding_dropout(self, embed, words, p=0.1):
         """
         TODO: re-write and add test
         """
@@ -169,16 +188,55 @@ class AWD_LSTM(nn.Module):
         return X
 
 
+    def activation_reg(self, alpha):
+        """
+        Calulates a regularisation factor that increases with magnitude of
+        activations from the final recurrent layer.
+        See section 4.6 of paper: https://arxiv.org/abs/1708.02182
+        
+        Returns an integer that can be added to loss after each forward pass.
+        """
+        # The authors report that they apply the L_2 norm denoted ||.||_2
+        # but they actually implement code equivalent to the following
+        # which is missig a square root operation:
+        # ar = sum(alpha * a.pow(2).mean() for a in self.output)
+        # return ar
+        # I assume that the mean is intended to be across timesteps
+        # and also across items in this batch
+        # So we want the mean L2 norm across all timesteps, across all
+        # items in this mini-batch. Verbosely this can be written as:
+        # ar = [th.sqrt(th.sum(a.pow(2), dim=1)).mean() for a in self.output]
+        # return alpha * (sum(ar)/len(ar)))
+        # Using pytorch's built in torch.norm 
+        masked_ht = self.output
+        L2_t = [th.norm(a, dim=1, p='fro').mean() for a in masked_ht]
+        return alpha * th.mean(T(L2_t)).item()
+
+
+    def temporal_activation_reg(self, beta):
+        # Not sure if this would be better returning average (as current) 
+        # or max?
+        ht = self.output_nodrop
+        L2_norms_t = []
+        for timestep in range(ht.size(0)-1):
+            diff = ht[timestep] - ht[timestep+1]
+            L2_norm_per_batch = th.norm(diff, dim=1, p='fro')
+            L2_norm_mean = th.mean(L2_norm_per_batch).item()
+            L2_norms_t.append(L2_norm_mean)
+        
+        return beta * th.mean(T(L2_norms_t))
+
+
     def forward(self, x, hiddens):
         # Translate input tokens to embedding vectors
         # with dropout
-        x = self.embedded_dropout(self.embedding, x, p=self.dropoute)
+        x = self.embedding_dropout(self.embedding, x, p=self.dropout_emb)
 
         # LSTM
         # --------------
         # Apply DropConnect to hidden-to-hidden weights 
         # once for each forward pass
-        self.drop_connect(p=self.dropout)
+        self.weight_dropout(p=self.dropout_wts)
 
 
         # At each timestep t, forward propagate down through layers
@@ -197,12 +255,13 @@ class AWD_LSTM(nn.Module):
             # .clone() is needed to avoid break in computation graph see:
             # https://discuss.pytorch.org/t/encounter-the-runtimeerror-one-of-the-variables-needed-for-gradient-computation-has-been-modified-by-an-inplace-operation/836
             zt_l0 = x[t,:,:].clone()
+            zt_l0 = self.varidrop_inp(zt_l0, t)
             zt_l1, (h[0,:,:], c[0,:,:]) = self.layer0(zt_l0, (h[0,:,:].clone(), c[0,:,:].clone()))
-            zt_l1 = self.varidrop_h(zt_l1, t)
+            zt_l1 = self.varidrop_hid(zt_l1, t)
             zt_l2, (h[1,:,:], c[1,:,:]) = self.layer1(zt_l1, (h[1,:,:].clone(), c[1,:,:].clone()))
-            zt_l2 = self.varidrop_h(zt_l2, t)
+            zt_l2 = self.varidrop_hid(zt_l2, t)
             zt_l3, (h[2,:,:], c[2,:,:]) = self.layer2(zt_l2, (h[2,:,:].clone(), c[2,:,:].clone()))
-            zt_l3 = self.varidrop_h(zt_l3, t)
+            zt_l3 = self.varidrop_out(zt_l3, t)
             # Record output from final layer at this timestep
             output = th.cat((output, zt_l3.unsqueeze(0)))
 
